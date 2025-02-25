009ebf9bf2e0f9a2cff8e2567842ca0480c50c34
ciemn001 <ciemn001@rangers.uwp.edu>
2025-02-25 09:20:35 -0600

    Pair programming example complete

---
+++ python/loops-and-lists/table_temperature_conversion_and_approximation.py
@@ -0,0 +1,12 @@
+temperature_interval = 10
+fahrenheit_temperature = 0
+maximum_temperature = 100
+
+print(f'T (°F)\t T (°C)\t T (°C_hat)')
+print(30 * "=")
+
+while fahrenheit_temperature <= maximum_temperature:
+    celsius_temperature = 5/9 * (fahrenheit_temperature - 32)
+    celsius_approximation = (fahrenheit_temperature - 30) / 2
+    print(f'{fahrenheit_temperature:6}\t{celsius_temperature:6.3f}\t{celsius_approximation:6.3f}')
+    fahrenheit_temperature += temperature_interval
