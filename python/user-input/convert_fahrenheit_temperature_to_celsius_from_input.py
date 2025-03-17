ea7a3165988d200548c68ad680e446a09a525e33
nateciemny <ciemn001@rangers.uwp.edu>
2025-03-16 20:46:01 -0500

    Update convert_fahrenheit_temperature_to_celsius_from_input.py

--- python/user-input/convert_fahrenheit_temperature_to_celsius_from_input.py
+++ python/user-input/convert_fahrenheit_temperature_to_celsius_from_input.py
@@ -1,1 +1,14 @@
+def fahrenheit_to_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius."""
+    celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
 
+if __name__ == '__main__':
+    # Ask the user for a temperature
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+
+    # Convert to Celsius
+    celsius = fahrenheit_to_celsius(fahrenheit)
+
+    # Print results
+    print(f"{fahrenheit}°F is equal to {celsius:.2f}°C")
