7df1d6c0d9b4d96072da0e88011b2be09cd983d4
nateciemny <ciemn001@rangers.uwp.edu>
2025-04-07 19:18:35 -0500

    Update make_hertzsprung_russell_diagram.py

--- python/dictionaries-and-strings/make_hertzsprung_russell_diagram.py
+++ python/dictionaries-and-strings/make_hertzsprung_russell_diagram.py
@@ -1,36 +1,14 @@
-"""
-make_hertzsprung_russell_diagram
-================================
-Generate the Hertzsprung-Russell diagram as depicted in this image:
-    https://en.wikipedia.org/wiki/Hertzsprung%E2%80%93Russell_diagram#/media/File:HRDiagram.png
-
-"""
+"""Generate Hertzsprung-Russell diagram like:
+    https://en.wikipedia.org/wiki/Hertzsprung%E2%80%93Russell_diagram#/media/File:HRDiagram.png"""
 import numpy as np
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 
 astronomical_unit = 1.495978707e11  # meters
-meters_to_light_years = 1. / 9.4607e15
+meters_to_light_years = 1./9.4607e15
 
 
 def star_colormap(star_b_minus_vs):
-    """
-    Scales the star B-V values to colors in the color map
-
-    Parameters
-    ----------
-    star_b_minus_vs : numpy.ndarray
-        An array of the star B-V values to scale
-
-    Returns
-    -------
-    tuple of numpy.ndarray
-    scaled_b_minus_vs : numpy.ndarray
-        Star B-V values scaled to the new limits
-    new_colormap : matplotlib.colors.ListedColormap
-        Color map of scaled star B-V values for matplotlib to use
-    """
-
     # Create color map from B-V = -0.33 (#7070ff) to 1.40 (#ff7f7f)
     # yellow = #ffff7f at B-V = 0.81
     number_of_gradient_points = 256
@@ -78,68 +56,40 @@ def star_colormap(star_b_minus_vs):
 
 
 def parallax_to_distance(parallax):
-    """
-    Convert parallax in milliarcseconds to distance in meters.
-
-    Parameters
-    ----------
-    parallax : float
-        Parallax value in milliarcseconds.
-
-    Returns
-    -------
-    distance : float
-        Distance corresponding to the given parallax, in meters.
-    """
-
+    """Take parallax in milliarcseconds and convert to distance in meters"""
     parallax_in_radians = (parallax / 1000. / 3600.) * (2 * np.pi / 360.)
     distance = astronomical_unit / np.tan(parallax_in_radians)
     return distance
 
 
 def apparent_to_absolute_magnitude(apparent_magnitude, distance):
-    """
-    Calculate absolute magnitude from apparent magnitude and distance.
-
-    Parameters
-    ----------
-    apparent_magnitude : float
-        The apparent magnitude of the celestial object.
-
-    distance : float
-        The distance to the object in meters.
-
-    Returns
-    -------
-    absolute_magnitude : float
-        The absolute magnitude of the celestial object.
-    """
+    """Calculate absolute magnitude from apparent magnitude and distance in meters"""
     distance_in_parsecs = distance / (648000. * astronomical_unit / np.pi)
-    absolute_magnitude = apparent_magnitude - 5 * np.log10(distance_in_parsecs) + 5
+    absolute_magnitude = apparent_magnitude - 5*np.log10(distance_in_parsecs) + 5
     return absolute_magnitude
 
 
 def read_file(filename):
-    """
-    Read four column data from HIPPARCOS satellite and return a nested dictionary
-
-    Parameters
-    ----------
-    filename : str
-        Name of the file with the Hipparcos satellite data
-
-    Returns
-    -------
-    hipparcos_data : dict
-        Dictionary of Hipparcos satellite data
-    """
+    """Read four column data from HIPPARCOS satellite and return a nested dictionary"""
     # Read in as nested dictionary
     # hipparcos_data = {'(star catalog number':
     #                       { 'parallax' : ... , 'apparent_magnitude' : ... , 'blue_minus_visual' : ... },
     #   ... }
-    # Remove the following line once you have completed the above
     hipparcos_data = {}
-
+    with open(filename, 'r') as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) != 4:
+                continue
+            star_id = parts[0]
+            parallax = float(parts[1])
+            apparent_mag = float(parts[2])
+            b_minus_v = float(parts[3])
+            hipparcos_data[star_id] = {
+                'parallax': parallax,
+                'apparent_magnitude': apparent_mag,
+                'blue_minus_visual': b_minus_v
+            }
     return hipparcos_data
 
 
@@ -155,11 +105,22 @@ if __name__ == '__main__':
     #       Append B-V value for current star into NumPy array of B-V values
     #           named star_b_minus_vs
 
-    # Remove the following two lines once you have created them above
-    star_absolute_magnitudes = np.array([])
-    star_b_minus_vs = np.array([])
+    star_absolute_magnitudes = []
+    star_b_minus_vs = []
 
-    # Use dark style for plot
+    for star_id, data in hipparcos_dictionary.items():
+        try:
+            distance = parallax_to_distance(data['parallax'])
+            abs_mag = apparent_to_absolute_magnitude(data['apparent_magnitude'], distance)
+            star_absolute_magnitudes.append(abs_mag)
+            star_b_minus_vs.append(data['blue_minus_visual'])
+        except Exception:
+            continue  
+
+    star_absolute_magnitudes = np.array(star_absolute_magnitudes)
+    star_b_minus_vs = np.array(star_b_minus_vs)
+
+    # Background
     plt.style.use('dark_background')
 
     # Reverse the absolute magnitude so that negative values appear on top
@@ -169,9 +130,24 @@ if __name__ == '__main__':
     scaled_b_minus_v, hr_diagram_colormap = star_colormap(star_b_minus_vs)
 
     # Create axes labels
+    plt.xlabel('B-V Color Index')
+    plt.ylabel('Absolute Magnitude')
+    plt.title('Hertzsprung-Russell Diagram')
 
-    # Make the axes identical to the model figure referenced at the top of this file
+    # Axis
+    plt.xlim(-0.5, 2.0)
+    plt.ylim(20, -10)
 
+    # Define size of points
+    plt.scatter(star_b_minus_vs, star_absolute_magnitudes,
+                c=scaled_b_minus_v, cmap=hr_diagram_colormap,
+                s=1, edgecolors='none')
+
+    # Output
+    plt.text(-0.45, 21, 'Created by Nate Ciemny', fontsize=8, color='white')
+
+    plt.tight_layout()
+    plt.show()
     # Define the scatter marker size in points squared (make it similar to the model figure)
 
     # Scatter plot of B-V vs absolute magnitude
