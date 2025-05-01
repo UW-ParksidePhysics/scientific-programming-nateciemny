a5ea357870257b7cdbfe37fb5b731ce5ddb66965
ciemn001 <ciemn001@rangers.uwp.edu>
2025-05-01 08:18:41 -0500

    push

--- python/project/planet_distance.py
+++ python/project/planet_distance.py
@@ -1,12 +1,14 @@
 """
-This code takes the distances between each planet and creates an icon representing each planet.
+This code makes scale models of the distance planets are from the Sun using a ruler. There are a total of 44 output
+png files for the different versions of the ruler. There are a total of 11 objects that I have coded the distances for.
+There are four variants for each object. The variants are as follows: a 3.5 inch black and white model, a 1.5 inch black
+and white model, a 3.5 inch colored model, and a 1.5 inch colored model. This program ensures that it is compatible with
+Mac and Windows for convenience. The diameters, in size, are relative to jupiter in order to create consistent markers.
+ The main goal of all this code is to give a brief visual for how far each planet is from the sun in astronomical units.
 
-https://github.com/williamdparker/AstronomyCalculations/blob/master/solar_system_sign_icons/draw_logarithmic_distance_ruler.py
-Black and white and color version. Overlay image of planet of interest. Label unit of the ruler (astronomical unit). Give conversion
-in miles and kilometers. Make 1.5 in. version and 3.5 in. version.
+ The code was made originally by Professor William Parker in this folder:
 https://github.com/williamdparker/AstronomyCalculations/tree/master/solar_system_sign_icons
-
-
+I have made edits in order to create multiple variants of the ruler.
 
 """
 __author__ = "Nate Ciemny"
@@ -18,14 +20,42 @@ import json
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
+import platform
 
 def read_json_file(data_file):
-    # Open and read the JSON file
+    """
+    Read and return the data from given JSON file.
+
+    Args:
+        data_file (str): Path to the JSON file.
+
+    Returns:
+        dict: Dictionary of data from JSON file.
+    """
+
     with open(data_file, "r") as file:
         json_data = json.load(file)
     return json_data
 
-def draw_logarithmic_ruler(object_distances, object_diameters, planet_colors, highlight_object):
+def draw_logarithmic_ruler(object_distances, object_diameters, planet_colors, highlight_object, grayscale=False, fig_height=5):
+    """
+  Draw distance ruler with planets scaled to relative diameters.
+
+  Args:
+      object_distances (dict): Dictionary of distances between planets.
+      object_diameters (dict): Dictionary of diameters between planets.
+      planet_colors (dict): Dictionary of colors between planets.
+      highlight_object (str): Highlight object.
+      grayscale (bool): If True, convert to grayscale.
+      fig_height (int): Figure height in inches.
+
+    Returns:
+        None. Saves file for each version of ruler.
+
+    """
+    # Scaling factor based on figure height
+    scale = fig_height / 5 # Scaling factor relative to 5 inch base
+
     # Calculate the extreme distance values
     min_distance = min(object_distances.values())
     max_distance = max(object_distances.values())
@@ -35,10 +65,9 @@ def draw_logarithmic_ruler(object_distances, object_diameters, planet_colors, hi
     log_max = np.ceil(np.log10(max_distance))
 
     # Generate the logarithmic ruler
-    fig, ax = plt.subplots(figsize=(10, 5))
-    # ax.set_xscale('log')
+    fig, ax = plt.subplots(figsize=(10, fig_height))
     ax.set_xlim(log_min, log_max)
-    ax.set_ylim(-0.7, 0.5)  # extended downward to make room for legend
+    ax.set_ylim(-0.7, 0.5)
 
     x_limits = ax.get_xlim()
     y_limits = ax.get_ylim()
@@ -52,18 +81,17 @@ def draw_logarithmic_ruler(object_distances, object_diameters, planet_colors, hi
     ax.get_yaxis().set_visible(False)
 
     # Draw rectangle representing the ruler background
-    ax.add_patch(plt.Rectangle((x_limits[0], y_limits[0]), x_range, 0.5*y_range, edgecolor='black',
-                                facecolor='none', linewidth=5))
+    ax.add_patch(plt.Rectangle((x_limits[0], y_limits[0]), x_range, 0.5 * y_range, edgecolor='black',
+                                facecolor='none', linewidth=5 * scale))
 
     # Label the ruler units as AU
     ax.text(log_min + 0.1, -0.35, 'AU',
-            ha='left', va='center', fontsize=18, fontname='American Typewriter', color='black')
+            ha='left', va='center', fontsize=18 * scale, fontname='Arial', color='black')
 
     # Add axis at top of rectangle
-    ax_top = ax.inset_axes([x_limits[0], y_limits[0] + 0.49*y_range, x_range, 0.001],
+    ax_top = ax.inset_axes([x_limits[0], y_limits[0] + 0.49 * y_range, x_range, 0.001],
                            transform=ax.transData, zorder=2)
 
-    # Hide the y-axis and unnecessary spines
     ax_top.get_yaxis().set_visible(False)
     ax_top.spines['top'].set_visible(False)
     ax_top.spines['right'].set_visible(False)
@@ -73,12 +101,12 @@ def draw_logarithmic_ruler(object_distances, object_diameters, planet_colors, hi
     ax_top.xaxis.set_major_locator(ticker.LogLocator(base=10.0, numticks=10))
     ax_top.set_xticks([np.log10(0.2), np.log10(0.5), 0, np.log10(2), np.log10(5), 1, np.log10(20), np.log10(50)])
     ax_top.set_xticklabels(['0.2', '0.5', '1', '2', '5', '10', '20', '50'])
-    ax_top.tick_params(direction='out', length=10, width=2, colors='black', pad=5,
+    ax_top.tick_params(direction='out', length=10 * scale, width=2 * scale, colors='black', pad=5 * scale,
                        top=False, bottom=True, labeltop=False, labelbottom=True,
-                       labelsize=24)
+                       labelsize=24 * scale)
 
     for tick in ax_top.get_xticklabels():
-        tick.set_fontname('American Typewriter')
+        tick.set_fontname('Arial')
 
     # Available Fonts
     # ['STIXSizeFourSym', 'DejaVu Sans Display', 'STIXNonUnicode', 'cmss10', 'STIXNonUnicode', 'DejaVu Sans Mono',
@@ -149,74 +177,87 @@ def draw_logarithmic_ruler(object_distances, object_diameters, planet_colors, hi
     # for obj, dist in object_distances.items():
     #     if log_min <= np.log10(dist) <= log_max:
     #         ax.annotate(obj, (dist, -0.25), ha='center', va='top', fontsize=8, rotation=45)
-    jupiter_diameter = 1.42984e5
-    giant_planet_reduction = 8
-    small_planet_reduction = 3
+    jupiter_diameter = 1.42984e5  # Km
+    giant_planet_reduction = 8  # Scale for large planets
+    small_planet_reduction = 3  # Scale for small planets
 
     for obj, dist in object_distances.items():
         if obj in object_diameters:
             diameter = object_diameters[obj]
             if log_min <= np.log10(dist) <= log_max:
-                # Calculate radius without log scaling
                 if diameter < 0.1 * jupiter_diameter:
                     radius = diameter / (small_planet_reduction * jupiter_diameter)
                 else:
                     radius = diameter / (giant_planet_reduction * jupiter_diameter)
 
                 planet_color = planet_colors.get(obj, "black")
+                if grayscale:
+                    planet_color = "black"
 
                 if obj == highlight_object:
                     ax.add_patch(plt.Circle((np.log10(dist), 0.32), radius, color=planet_color, alpha=0.9))
+
                     label_y = 0.36
                     if obj in ["Jupiter", "Saturn"]:
-                        label_y = 0.42  # Saturn label
-                        if obj == "Jupiter":
-                            label_y = 0.45 # Jupiter label
+                        label_y = 0.42
+                    if obj == "Jupiter":
+                        label_y = 0.45
+
+                    label_color = planet_color if not grayscale else "black"
 
                     ax.text(np.log10(dist), label_y, obj, ha='center', va='bottom',
-                            color=planet_color, fontsize=14, fontname='American Typewriter')
+                            color=label_color, fontsize=14 * scale, fontname='Arial')
 
-                    ax.annotate('', xy=(np.log10(dist), -0.11), xytext=(np.log10(dist), 0.32 - radius),
-                                arrowprops=dict(facecolor=planet_color, shrink=0.05, width=2, headwidth=8))
+                    ax.annotate('', xy=(np.log10(dist), -0.12),
+                                xytext=(np.log10(dist), 0.32 - radius),
+                                arrowprops=dict(facecolor=planet_color, edgecolor=planet_color, shrink=0.05,
+                                                width=2 * scale,
+                                                headwidth=8 * scale))
 
                 else:
                     ax.add_patch(plt.Circle((np.log10(dist), 0.05), radius, color='black', alpha=0.9))
 
     # Add conversion legend for AU to miles and kilometers
     ax.text(log_min + 0.1, -0.55,
-            "1 AU ≈ 92.96 million mi.\n1 AU ≈ 149.6 million km",
-            ha='left', va='top', fontsize=14, fontname='American Typewriter')
+            "1 AU ≈ 92.96 million miles ≈ 149.6 million kilometers",
+            ha='left', va='top', fontsize=14 * scale, fontname='Arial')
 
     ax.set_aspect('equal', 'box')
     ax.set_frame_on(False)
 
-    plt.savefig(f"{highlight_object}.png", dpi=300, bbox_inches='tight')
+    suffix = "_bw" if grayscale else "_color"
+    plt.savefig(f"{highlight_object}{suffix}_{fig_height}in.png", dpi=300, bbox_inches='tight')
     plt.close()
 
 
 if __name__ == '__main__':
     import matplotlib
+    import platform
 
-    matplotlib.use('macosx')
+    if platform.system() == 'Darwin':  # Mac
+        matplotlib.use('macosx')
+    else:  # Windows or Linux
+        matplotlib.use('Agg')
 
     planetary_distances = read_json_file("semimajor_axes.json")
     planetary_diameters = read_json_file("diameters.json")
     planet_colors = read_json_file("planet_colors.json")
 
     for planet in planetary_distances.keys():
-        draw_logarithmic_ruler(planetary_distances, planetary_diameters, planet_colors, highlight_object=planet)
+        for fig_height in [3.5, 1.5]:
+            draw_logarithmic_ruler(planetary_distances, planetary_diameters, planet_colors, highlight_object=planet,
+                                   grayscale=False, fig_height=fig_height)
+            draw_logarithmic_ruler(planetary_distances, planetary_diameters, planet_colors, highlight_object=planet,
+                                   grayscale=True, fig_height=fig_height)
 
 # test_plot()
-
-# *** Will add a loop that prints out multiple visualizations. There will be 44 total including 22 just like this one
-# in black and white. There will be another 22 that would be the same but in 1.5 inches. ***
 
 #### RENAME from planet_distance.py to (your_project_short_name).py
 # File structure
 # 1. Commented paragraph describing project ~ 100-200 words
 # 2. Module imports that are used in multiple functions
 # 3. Function definitions
-# 4. if __name__ == "__main__" block, which calls a primary function with a clear name 
+# 4. if __name__ == "__main__" block, which calls a primary function with a clear name
 
 # All code is inside function definitions for simulation solution & visualization (functional programming)
 #	Each function contains a docstring compliant with PEP 257: https://www.python.org/dev/peps/pep-0257/
@@ -226,15 +267,15 @@ if __name__ == '__main__':
 #	1. Module imports
 #		Use SciPy constants for physical constants in particular function (not globally)
 #			https://docs.scipy.org/doc/scipy/reference/constants.html
-#		Follow best practice order: 
+#		Follow best practice order:
 #			https://docs.python.org/3/faq/programming.html#what-are-the-best-practices-for-using-import-in-a-module
 # 	2. Simulation parameters
 #		Each parameter named clearly and units marked in in-line comment
-#		Naming of all variables should comply with PEP 8: 
+#		Naming of all variables should comply with PEP 8:
 #			https://www.python.org/dev/peps/pep-0008/#documentation-strings
 #			(lower_case_with_underscores)
 # 	3. Computed parameters (from simulation parameters)
 # 	4. Function calls (use PEP 8-compliant lower_case_with_underscores) and simple calculations for:
 #		data read-in
-#		simulation solution 
+#		simulation solution
 #		visualization
